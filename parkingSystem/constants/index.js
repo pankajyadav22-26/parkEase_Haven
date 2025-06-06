0a07a1bd9b297964201c46f4681ae6e46f1db42a
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-06-06 23:25:10 +0530

    backend_url updated

--- parkingSystem/constants/index.js
+++ parkingSystem/constants/index.js
@@ -1,7 +1,7 @@
 
 import { COLORS,  SIZES, images, SHADOWS} from "./theme";
 
-const backendUrl = "http://192.168.0.115:3000";
+const backendUrl = "https://parkease-haven.onrender.com";
 const GATE_COORDS = {
     latitude: 28.8167148,
     longitude: 77.1330412,
