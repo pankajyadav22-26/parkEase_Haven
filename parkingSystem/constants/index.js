07e219b2803d6e9ad507c96ad92e659e444b46c2
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2026-03-12 15:08:11 +0530

    frontend update

--- parkingSystem/constants/index.js
+++ parkingSystem/constants/index.js
@@ -1,6 +1,6 @@
 
 import { COLORS,  SIZES, images, SHADOWS} from "./theme";
 
-const backendUrl = "http://192.168.1.37:3000";
+const backendUrl = "https://parkease-haven.onrender.com";
 
 export { COLORS,  SIZES, images, SHADOWS, backendUrl};
